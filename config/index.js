538e970dacf7ad737b977740ce988427395bb5c4
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2021-07-21 17:57:55 +0200

    done for now

---
+++ config/index.js
@@ -0,0 +1,113 @@
+module.exports = {
+	//-- SITE SETTINGS -----
+	author: "@rayremnant",
+	siteTitle: "Ray Remnant",
+	siteShortTitle: "Ray Remnant", // Used as logo text in header, footer, and splash screen
+	siteDescription:
+		"A modern one-page portfolio with a clean yet expressive design.",
+	siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+	siteLanguage: "en_US",
+	siteIcon: "content/favicon.png", // Relative to gatsby-config file
+	seoTitleSuffix: "Ray Remnant", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+
+	// -- THEME SETTINGS -----
+	colors: {
+		lightTheme: {
+			primary: "#000000",
+			secondary: "#FFF4D9",
+			tertiary: "#F2F2F2",
+			text: "#000000",
+			subtext: "#555555",
+			background: "#FFFFFF",
+			card: "#FFFFFF",
+			scrollBar: "rgba(0, 0, 0, 0.5)",
+			boxShadow: "rgba(0, 0, 0, 0.16)",
+			boxShadowHover: "rgba(0, 0, 0, 0.32)",
+		},
+		darkTheme: {
+			primary: "#FAFAFA",
+			secondary: "#2A2926",
+			tertiary: "#252525",
+			text: "rgba(255, 255, 255, 0.87)",
+			subtext: "#AAAAAA",
+			background: "#121212",
+			card: "#1C1C1C",
+			scrollBar: "rgba(255, 255, 255, 0.5)",
+			boxShadow: "rgba(0, 0, 0, 0.16)",
+			boxShadowHover: "rgba(0, 0, 0, 0.32)",
+		},
+	},
+	fonts: {
+		primary: "Questrial, Arial, sans-serif",
+	},
+
+	//-- ARTICLES SECTION SETTINGS -----
+	// You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+	// To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+	mediumRssFeed:
+		"https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+	// rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+	shownArticles: 3,
+
+	//-- SOCIAL MEDIA SETTINGS -----
+	// There are icons available for the following platforms:
+	// Medium, GitHub, LinkedIn, XING, Behance, E-Mail
+	socialMedia: [
+		{
+			name: "Linkedin",
+			url: "https://www.linkedin.com/in/florin-dragos/",
+		},
+		/* 		{
+					name: "Medium",
+					url: "https://konstantinmuenster.medium.com/",
+				}, */
+		{
+			name: "Github",
+			url:
+				"https://github.com/RayRemnant",
+		},
+		/* 		{
+					name: "Behance",
+					url: "https://www.behance.net/konstanmnster",
+				}, */
+		{
+			name: "Mail",
+			url: "mailto:rayremnant.me@gmail.com?subject=Work? Volunteer?&body=Hi Ray, I'd like to talk with you about...",
+		},
+	],
+
+	//-- NAVIGATION SETTINGS -----
+	navLinks: {
+		menu: [
+			{
+				name: "About Me",
+				url: "/#about",
+			},
+			{
+				name: "Projects",
+				url: "/#projects",
+			},
+			{
+				name: "Contact",
+				url: "/#contact",
+			},
+		],
+		button: {
+			useFileName: false,
+			name: "Resume",
+			fileName: "", // the file has to be placed inside the static folder at the root level
+			url: "https://drive.google.com/file/d/1UoSJl7KYtw49h3694BWr9n6wRncP3u7w/view?usp=sharing", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+		},
+	},
+	/* footerLinks: [
+		{
+			name: "Privacy",
+			url: "/privacy",
+		},
+		{
+			name: "Imprint",
+			url: "/imprint",
+		},
+	], */
+}
